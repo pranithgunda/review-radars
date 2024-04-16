9bf8277670b6dace99d3b13537db3ba167509a1d
Pranith Gunda <pranith0527@yahoo.com>
2024-04-15 23:14:03 -0400

    JavaScript in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
-const productName = document.getElementById('product-name');
+const searchText = document.getElementById('product-name');
 const searchButton = document.getElementById('search-button');
 
-function searchProducts(){
+// add Click event on search button
 
-    const S
+searchButton.addEventListener('click', function(event){
+    const productName = searchText.value;
+    const requestURL = `https://amazon-product-data6.p.rapidapi.com/product-by-text?keyword=${productName}&page=1&country=US&sort_by=feature`;
+    const options = {
+        method:'GET',
+        headers:{
+            'X-RapidAPI-Key':'77c3077d56mshb8de8e859891fdep1619d0jsn65f6d9cb5161',
+            'X-RapidAPI-Host':'amazon-product-data6.p.rapidapi.com'
+        }
+    };
 
-    //search for the product user is looking for
-    const requestUrl = ''
-}
+    fetch(requestURL,options)
+    .then(function (response) {
+        console.log(response.json());
+    })
+})
